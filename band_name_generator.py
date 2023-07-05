163382084bae98f2dff2b09b90f0c043148aebba
JTanner04 <138722231+JTanner04@users.noreply.github.com>
2023-07-05 13:22:49 -0700

    Delete band_name_generator.py

--- band_name_generator.py
+++
@@ -1,7 +0,0 @@
-print("Hello my people!")
-
-grow = input("What city did you grow up in?\n")
-
-pet = input("what is the name of a pet?\n")
-
-print("Here is your band name "+grow + " " + pet +"\n")
